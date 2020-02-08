36b415e8eca5e9cd5c626aecab2162390e6f9010
WaleedTiti99 <waleed.titi8@gmail.com>
2020-02-08 20:48:11 +0200

    firstCommit

---
+++ src/addition/addition.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class addition {
+	public static int add(int x, int y) {
+		return x+y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(4,5));
+
+	}
+
+}
